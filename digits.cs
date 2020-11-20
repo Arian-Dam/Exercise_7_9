3f61e944d77bc0ac302698847e8ff297cb9677b5
Arian <zeusarian@hotmail.com>
2020-11-20 22:24:12 +0100

    Sort the digits file

--- digits.cs
+++ digits.cs
@@ -1,35 +1,39 @@
+/* 
+ calculates how many numbers of 1, 2, 3 or more digits are
+ entered by the user until he/she types “end”.
+ */
 using System;
 
 class digits
 {
- static void Main()
- {
-	 //counters of number of digits a is for one digit, b is for 2 digits, c is for 3 digits and d is for more than 3 digits
-     int a = 0, b = 0, c = 0, d = 0;
-      Console.Write("Insert a number: ");
-      // read the number as string to check the word "end"
-      string e = Console.ReadLine();
-     while(e!="end"){
-int input = Convert.ToInt32(e);
-       if(input != 0)
-{
-       if(input/10 == 0)
-           a ++;  // it only has one digit
-       else 
-       if (input/100 == 0)
-		b ++;  // the number has 2 digits
-        else
-        if (input/1000 == 0)
-        c ++;  // the number has 3 digits
-        else
-           d++;  // the number has more than 3 digits
-        }
-      e = Console.ReadLine();
+	static void Main()
+	{
+		int one_digit = 0, two_digit= 0, three_digit= 0, more_that4 = 0,converted_string;
+		string condition_finish;
+		
+		Console.Write("insert numbers and when you want to finish type final: ");
+		condition_finish = Console.ReadLine();
+		
+		while(condition_finish!="end")
+		{
+			converted_string = Convert.ToInt32(condition_finish);
+			
+			if(converted_string != 0)
+			{
+				if(converted_string/10 == 0)
+					one_digit ++;  
+					else 
+						if (converted_string/100 == 0)
+							two_digit ++; 
+								else
+									if (converted_string/1000 == 0)
+										three_digit ++;  
+										else
+											more_that4++;  
+			}
+		condition_finish = Console.ReadLine();
+		}
+		Console.WriteLine("one: {0} two: {1} three: {2} more: {3}",
+			one_digit,	two_digit, three_digit, more_that4); 
+	}
 }
-
-Console.WriteLine("one: {0} two: {1} three: {2} more: {3}", a, b, c, d); 
-
-
- }
-}
-
